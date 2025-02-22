8ce6bcd4f8910fb2bc114468f05cdd783c69b1f4
LMK <mirrol33@gmail.com>
2025-02-22 19:07:12 +0900

    02.22 update3

--- 3차프로젝트/3.구현소스/outfront/src/components/pages/Main.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/pages/Main.jsx
@@ -1,21 +1,43 @@
 /// 메인페이지 컴포넌트 : Main.jsx ///
-
+import React, { useState, useEffect } from "react";
 import "../../scss/main.scss";
 
-export default function Main(){
-  // 리턴 코드구역 ////
+const Main = () => {
+  const [eduList, setEduList] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/edu_data.json")
+      .then((res) => res.json())
+      .then(setEduList)
+      .catch(console.error);
+  }, []);
+
   return (
-    <>
-      <main>
-        <div className="edu-wrap">
-          <ul className="edu-list">
-            <li>
-              <img src="" />
-              <h2></h2>
-            </li>
-          </ul>
-        </div>
-      </main>
-    </>
+    <main className="main">
+      <h1>🎓 교육 강의 목록</h1>
+      <ul className="list-wrap">
+        {eduList.map((edu) => (
+          <li key={edu.idx} className="edu-list">
+            <article>
+              <picture>
+                <img
+                  src={`/images/edu_thumb/${edu.idx}.png`}
+                  alt={`교육 이미지 ${edu.idx}`}
+                />
+              </picture>
+            </article>
+            <h3>{edu.gName}</h3>
+            <p>레벨: {edu.gLevel}</p>
+            <p>가격: {edu.gPrice}</p>
+            <span className="hover-txt">
+              <p>설명: {edu.gInfo}</p>
+              <p>분류: {edu.gCate}</p>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
-} //// Main 컴포넌트 ////
+};
+
+export default Main;
