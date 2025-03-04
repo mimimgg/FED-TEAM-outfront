256bc8b44872b7c1beaee93c0e93ba3c1402d3ee
mimimgg <com42123@gmail.com>
2025-03-04 10:40:42 +0900

    상단 미디어쿼리 중..

--- 3차프로젝트/3.구현소스/outfront/src/js/top_area.js
+++ 3차프로젝트/3.구현소스/outfront/src/js/top_area.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import $ from 'jquery';
 
 // ** 햄버거 버튼 클릭 시 GNB 메뉴 노출 ** //
-$(".top-menu-btn").click(function () {
-  $(".gnb").toggle();
-});
+$(() => (
+  $(".top-menu-btn").click(function () {
+    $(".gnb").toggle();
+  })
+));
