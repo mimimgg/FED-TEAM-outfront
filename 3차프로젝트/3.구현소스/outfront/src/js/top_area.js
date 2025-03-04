eecbbbc79491faa6bad881a298e12b91be2c2ce4
Tom B. Parker <tombap8@hotmail.com>
2025-03-04 17:59:18 +0900

    ss

--- 3차프로젝트/3.구현소스/outfront/src/js/top_area.js
+++ 3차프로젝트/3.구현소스/outfront/src/js/top_area.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 // ** 햄버거 버튼 클릭 시 GNB 메뉴 노출 ** //
 $(() => (
-  $(".top-menu-btn").click(function () {
-    $(".gnb").toggle();
+  $(".top-menu-btn").on("click",function () {
+    $(".gnb").toggleClass('on');
   })
 ));
