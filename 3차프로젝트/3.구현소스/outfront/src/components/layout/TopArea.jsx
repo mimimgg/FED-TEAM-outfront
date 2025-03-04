a471d0a04d9ce61e2224009ad7d9279800c5ca9d
mimimgg <com42123@gmail.com>
2025-03-04 22:37:25 +0900

    max-width 수정

--- 3차프로젝트/3.구현소스/outfront/src/components/layout/TopArea.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/layout/TopArea.jsx
@@ -18,76 +18,78 @@ export default function TopArea() {
     <>
       {/* 로고 */}
       <header className="top-area">
-        <div className="navbar-left">
-          <h1 className="brand_logo">
-            <a href="#">
-              <img src="../../images/common/brand_logo.png" alt="아웃프런 브랜드 로고" />
+        <div className="top-wrap">
+          <div className="navbar-left">
+            <h1 className="brand_logo">
+              <a href="#">
+                <img src="../../images/common/brand_logo.png" alt="아웃프런 브랜드 로고" />
+              </a>
+            </h1>
+            <a href="#" className="top-menu-btn">
+              <i className="fa-solid fa-bars"></i>
             </a>
-          </h1>
-          <a href="#" className="top-menu-btn">
-            <i className="fa-solid fa-bars"></i>
-          </a>
 
-          {/* 네비게이션 */}
-          <nav className="gnb">
-            <ul>
-              {/* gnb 메뉴 맵 바인딩 */}
-              {menu.map((v, i) => (
-                <li key={i}>
-                  {<Link to={v.link}>{v.txt}</Link>}
-                  {
-                    // 서브메뉴가 있는 경우 출력하기
-                    v.sub && (
-                      <div className="smenu">
-                        <ol>
-                          {v.sub.map((v, i) => (
-                            <li key={i}>
-                              <Link to={v.link}>{v.txt}</Link>
-                            </li>
-                          ))}
-                        </ol>
-                      </div>
-                    )
-                  }
-                </li>
-              ))}
-            </ul>
-          </nav>
-          {/* 모바일 네비게이션 */}
-        </div>
-
-        {/* 검색바 */}
-        <div className="navbar-right">
-          <div className="search-bar">
-            <input
-              type="search"
-              id="user-search"
-              name="query"
-              placeholder="나의 진짜 성장을 도와줄 실무 강의를 찾아보세요"
-            />
-            <button className="search-icon" type="submit">
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
+            {/* 네비게이션 */}
+            <nav className="gnb">
+              <ul>
+                {/* gnb 메뉴 맵 바인딩 */}
+                {menu.map((v, i) => (
+                  <li key={i}>
+                    {<Link to={v.link}>{v.txt}</Link>}
+                    {
+                      // 서브메뉴가 있는 경우 출력하기
+                      v.sub && (
+                        <div className="smenu">
+                          <ol>
+                            {v.sub.map((v, i) => (
+                              <li key={i}>
+                                <Link to={v.link}>{v.txt}</Link>
+                              </li>
+                            ))}
+                          </ol>
+                        </div>
+                      )
+                    }
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            {/* 모바일 네비게이션 */}
           </div>
 
-          {/* 로그인 / 회원가입 */}
-          <ul className="right-icon">
-            <li>
-              <a className="login-btn top-btn" href="#">
-                <i className="fa-solid fa-right-to-bracket"></i>
-              </a>
-            </li>
-            <li>
-              <a className="cart-btn top-btn" href="#">
-                <i className="fa-solid fa-cart-shopping"></i>
-              </a>
-            </li>
-            <li>
-              <a className="user-btn top-btn" href="#">
-                <i className="fa-regular fa-user"></i>
-              </a>
-            </li>
-          </ul>
+          {/* 검색바 */}
+          <div className="navbar-right">
+            <div className="search-bar">
+              <input
+                type="search"
+                id="user-search"
+                name="query"
+                placeholder="나의 진짜 성장을 도와줄 실무 강의를 찾아보세요"
+              />
+              <button className="search-icon" type="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </div>
+
+            {/* 로그인 / 회원가입 */}
+            <ul className="right-icon">
+              <li>
+                <a className="login-btn top-btn" href="#">
+                  <i className="fa-solid fa-right-to-bracket"></i>
+                </a>
+              </li>
+              <li>
+                <a className="cart-btn top-btn" href="#">
+                  <i className="fa-solid fa-cart-shopping"></i>
+                </a>
+              </li>
+              <li>
+                <a className="user-btn top-btn" href="#">
+                  <i className="fa-regular fa-user"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
     </>
