8d8665d42b794750f008831ca4c40c680356309b
mimimgg <com42123@gmail.com>
2025-03-05 16:50:30 +0900

    수정

--- 3차프로젝트/3.구현소스/outfront/src/components/layout/FooterArea.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/layout/FooterArea.jsx
@@ -56,16 +56,16 @@ export default function FooterArea() {
             <div className="copyright">©OUTFRONT. ALL RIGHTS RESERVED</div>
           </div>
           <div className="footer-sns">
-            <a className="sns-facebook sns-icon" href="#">
+            <a className="sns-facebook sns-icon" href="https://www.facebook.com/inflearn">
               <i className="fa-brands fa-facebook-f"></i>
             </a>
-            <a className="sns-insta sns-icon" href="#">
+            <a className="sns-insta sns-icon" href="https://www.instagram.com/inflearn__official/#">
               <i className="fa-brands fa-instagram"></i>
             </a>
-            <a className="sns-youtube sns-icon" href="#">
+            <a className="sns-youtube sns-icon" href="https://www.youtube.com/channel/UC0Y0T9JpgIBbyGDjvy9PbOg">
               <i className="fa-brands fa-youtube"></i>
             </a>
-            <a className="sns-x sns-icon" href="#">
+            <a className="sns-x sns-icon" href="https://x.com/inflearn">
               <i className="fa-brands fa-x-twitter"></i>
             </a>
           </div>
