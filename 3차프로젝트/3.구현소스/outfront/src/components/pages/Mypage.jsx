5e04643c5cc84e92f55f4d8dcb4f52dd12e2b403
freelife11 <mirrol33@gmail.com>
2025-03-07 10:27:25 +0900

    03.07 update1

--- 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import "../../scss/mypage.scss";
+
 function Mypage() {
   return (
     <>
-      <h2>마이페이지</h2>
+      <div className="mypage-wrap">
+        <div className="mypage-top">
+          <h2>마이페이지</h2>
+          <span>ID : testtest</span>
+          <p>이민경님 아웃프런에 오신것을 환영합니다 :) <br />
+          당장 공부하지 하지않으면 너의 인생이 망할 수도 있다!!</p>
+        </div>
+        <hr />
+      </div>
+      
     </>
   );
 }
