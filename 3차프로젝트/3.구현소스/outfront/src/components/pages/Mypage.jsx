7144e6ddc75fbf23b4984cad2f0abcc8ab96aecb
freelife11 <mirrol33@gmail.com>
2025-03-07 10:40:55 +0900

    03.07 update2

--- 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
@@ -7,10 +7,13 @@ function Mypage() {
     <>
       <div className="mypage-wrap">
         <div className="mypage-top">
-          <h2>마이페이지</h2>
+          <h2>MYPAGE</h2>
+          <picture>
+            <img src="./images/mypage/1.png" alt="profile" />
+          </picture>
           <span>ID : testtest</span>
-          <p>이민경님 아웃프런에 오신것을 환영합니다 :) <br />
-          당장 공부하지 하지않으면 너의 인생이 망할 수도 있다!!</p>
+          <p><b>이민경</b>님 <b>아웃프런</b>에 오신것을 환영합니다 :) <br />
+          <b>당장 공부하지 하지않으면 너의 인생이 망할 수도 있다!!</b></p>
         </div>
         <hr />
       </div>
