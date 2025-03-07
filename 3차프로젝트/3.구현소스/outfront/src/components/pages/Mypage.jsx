45d4b41fd831d3f5471dc2acd8d34380ffd9fe3f
freelife11 <mirrol33@gmail.com>
2025-03-07 10:47:18 +0900

    03.07 update3

--- 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
+++ 3차프로젝트/3.구현소스/outfront/src/components/pages/Mypage.jsx
@@ -12,7 +12,7 @@ function Mypage() {
             <img src="./images/mypage/1.png" alt="profile" />
           </picture>
           <span>ID : testtest</span>
-          <p><b>이민경</b>님 <b>아웃프런</b>에 오신것을 환영합니다 :) <br />
+          <p><b>OOO</b>님 <b>아웃프런</b>에 오신것을 환영합니다 :) <br />
           <b>당장 공부하지 하지않으면 너의 인생이 망할 수도 있다!!</b></p>
         </div>
         <hr />
