2d50257dbc0f843b0c3bd6c7af48e76551798248
mimimgg <com42123@gmail.com>
2025-03-19 17:34:44 +0900

    cart 생성

---
+++ 3차프로젝트/3.구현소스/outfront/src/components/modules/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../scss/pages/cart.scss";
+
+export const Cart = () => {
+  return (
+    <>
+      <h2 className="title">수강바구니</h2>
+    </>
+  );
+}
+
+export default Cart;
